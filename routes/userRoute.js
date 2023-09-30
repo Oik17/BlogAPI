e04fd6360ec7a2df59060af522e956cd9b5a401c
Akshat Gupta <akshat.g1707@gmail.com>
2023-10-01 01:30:41 +0530

    login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-const {signup}=require('../controllers/userAuth')
+const {signup,login}=require('../controllers/userAuth')
 router.get('/test', (req, res) => {
   res.send("hello");
 });
@@ -13,4 +13,7 @@ router.post('/signup', async (req, res) => {
     signup(req,res);
   });
 
+router.post('/login', async (req,res)=>{
+    login(req,res);
+})
 module.exports=router;    
