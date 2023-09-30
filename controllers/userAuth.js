b6bf98f432175770780bcde9ad62ead51eb4618c
Akshat Gupta <akshat.g1707@gmail.com>
2023-10-01 01:30:41 +0530

    login

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -10,21 +10,45 @@ async function signup(req,res){
             
             return res.status(400).json({error: "User already exists"});
         }
-        
-        const hashedPassword=await bcrypt.hash(req.body.password,10);
-        const newUser=await user.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        newUser.save()
-        res.status(201).send("User created\n"+newUser)
+        else{
+            
+            const hashedPassword=await bcrypt.hash(req.body.password,10);
+            const newUser=await user.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            })
+            newUser.save()
+            res.status(201).send("User created\n"+newUser)
+        }
     }
     catch(err){
         res.status(500).send(err)
     }
 }
 
+async function login(req,res){
+    try{
+        const user_exists = await user.findOne({"email":req.body.email});
+
+        if(user_exists==null){
+            console.log("why am i here")
+            return res.status(400).json({message: "user does not exist"})
+        }
+
+        if(await bcrypt.compare(req.body.password,user_exists.password)){
+            res.status(201).send("Logging in");
+        }
+        else{
+            res.status(500).send("Incorrect password");
+        }
+        
+    } catch(err){
+        res.status(500).send(err);
+    }
+}
+
 module.exports={
-    signup
+    signup,
+    login
 }
